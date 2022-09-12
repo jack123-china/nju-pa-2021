14ac60b3827b9982ae29cf436b59e09fb00916a0
tracer-ics2021 <tracer@njuics.org>
2022-09-12 14:44:05 +0800

    >  compile 201220000    Static hostname: jackshi-MS-7A69          Icon name: computer-desktop            Chassis: desktop         Machine ID: dc795f71f50e4128a586238dcef21bc9            Boot ID: 19bdefd56cb449d784af0f75ae77d7b8   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  14:44:05 up 21:16,  1 user,  load average: 0.30, 0.35, 0.36

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -14,11 +14,9 @@ const char *regs[] = {
 void isa_reg_display() {
   
   int i = 0;
-  while(regs[i]){
-	  printf("=============%p \n", regs);
-//     printf("%s    ,%x        ,%d ", regs[i],cpu.gpr[i]._32, cpu.gpr[i]._32);
+  while(regs[i] != NULL ){
+     printf("%s    ,%x        ,%d ", regs[i],cpu.gpr[i]._32, cpu.gpr[i]._32);
 
-    printf("%s ",regs[i]);
     i++;
   }
 }
