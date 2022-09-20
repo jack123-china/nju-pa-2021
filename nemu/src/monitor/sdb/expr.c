6e78fa5250bfa35b33b035a38b0623cd8f170d67
tracer-ics2021 <tracer@njuics.org>
2022-09-20 16:13:18 +0800

    >  compile 201220000    Static hostname: jackshi-MS-7A69          Icon name: computer-desktop            Chassis: desktop         Machine ID: dc795f71f50e4128a586238dcef21bc9            Boot ID: 37c955219b27450db83144f4c67875c3   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  16:13:18 up 1 day,  9:22,  1 user,  load average: 0.42, 0.42, 0.52

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -151,6 +151,7 @@ word_t expr(char *e, bool *success) {
 
   char *str[32] ;
   for (int i = 0 ; i < 32;i++){
+      printf("init char = %d \n",i);
       memset(str[i] , 0 , 32);
   }
   for (int i = 0 ;i < nr_token;i++){
