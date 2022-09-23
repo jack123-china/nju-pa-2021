5f879dfdbb7330307b5316eacfff2d5d65538d1f
tracer-ics2021 <tracer@njuics.org>
2022-09-23 09:33:20 +0800

    >  compile 201220000    Static hostname: jackshi-MS-7A69          Icon name: computer-desktop            Chassis: desktop         Machine ID: dc795f71f50e4128a586238dcef21bc9            Boot ID: 37c955219b27450db83144f4c67875c3   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  09:33:20 up 4 days,  2:42,  1 user,  load average: 0.60, 0.49, 0.40

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -9,7 +9,7 @@ typedef struct watchpoint {
   /* TODO: Add more members if necessary */
   
   uint32_t lastResult ;
-  char *expr; 
+  char expr[32]; 
 } WP;
 
 static WP wp_pool[NR_WP] = {};
@@ -59,7 +59,7 @@ void add_head_wp( uint32_t last,char *expr){
    wp->next = head;
    head = wp->next;
    wp->lastResult = last;
-   wp->expr = expr;
+   strcpy(wp->expr,expr);
    return ;
 }
 
